2d0e8a695eec266a3c4929b568789e0a9e50d487
Maxtar <bmaxtar@gmail.com>
2020-11-23 17:02:57 +0000

    depot 2

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Notes from './components/Notes';
 import Questions from './components/Questions';
 import Add from './components/Add';
-
+import Card from './components/Card';
 
 function App() {
   return (
@@ -15,6 +15,7 @@ function App() {
         <Route  path="/Notes" component={Notes} />
         <Route  path="/Questions" component={Questions} />
         <Route  path="/Add" component={Add} />
+        <Route  path="/Card" component={Card} />
 
 
       </div>
---
+++ src/components/Card.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+function Card() {
+  return (
+    <div className="Card">
+      <div>
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
+              <div className="card card-signin my-5">
+                <div className="card-body">
+                  <p className="font-weight-bold text-center text-white"><h5>Question</h5></p>
+                  <button className="btn btn-lg btn-danger btn-block text-uppercase" type="submit">1-10</button>
+                  <div className="text text-light text-center "> <p>Lorem ipsum dolor sit amet,consecteur adipiscingelit.elit interdum ipsum posuere nunc fermenturn
+                    lorem ut amet proin.Molestie ul ultricies tortor.
+                  </p>
+                </div>
+                <form className="form-signin">
+                  <button className="btn btn-lg btn-success btn-block text-uppercase" type="submit">vrai</button>
+                  <button className="btn btn-lg btn-danger btn-block text-uppercase" type="submit"> Faut</button>
+                  <button className="btn btn-lg btn-light btn-block text-uppercase" type="submit"> Je  sais pas</button>
+                  </form>
+                  <div>
+             </div>
+             </div>
+             </div>
+             </div>
+             </div> 
+             </div>
+             </div>
+             </div>
+
+  );
+}
+ export default Card;
